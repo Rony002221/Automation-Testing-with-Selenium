7d551d2181dd24349ce7725ef1e1d993d623c85b
rony <rony49883@gmail.com>
2022-01-07 00:21:59 +0600

    add automation script for entering Radio Button, Skills, Country, Year, Month and Day

--- SeleniumWebdriverDemo/src/test/java/SeleniumTest.java
+++ SeleniumWebdriverDemo/src/test/java/SeleniumTest.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -35,6 +36,43 @@ public class SeleniumTest {
 		//Entering into Confirm Password
 		driver.findElement(By.xpath("*//input[@id='secondpassword']")).sendKeys("aBc@2022");
 						
+		//Select Radio Button
+		driver.findElement(By.xpath("*//input[@value = 'Male']")).click();
+		
+		//Select Radio Button
+		driver.findElement(By.xpath("*//input[@value='Cricket']")).click();
+		
+		//Select Skills
+		Select skills = new Select(driver.findElement(By.id("Skills")));
+		skills.selectByVisibleText("Java");
+		
+		
+		//Select Country
+		Select country = new Select(driver.findElement(By.id("countries")));
+		country.selectByVisibleText("BD");
+						
+		//Select Year
+		Select year = new Select(driver.findElement(By.id("yearbox")));
+		year.selectByVisibleText("1995");
+				
+		//Select Month
+		Select Month = new Select(driver.findElement(By.xpath("*//select[@placeholder = 'Month']")));
+		Month.selectByVisibleText("February");
+						
+		//Select Day
+		Select Day = new Select(driver.findElement(By.xpath("*//select[@placeholder = 'Day']")));
+		Day.selectByVisibleText("25");
+								
+						
+		
+		
+		
+		
+		//Select drop-down
+		driver.findElement(By.xpath("*//div[@id = 'msdd']")).click();
+		driver.findElement(By.xpath("*//div[@id = 'msdd']/../div[2]/ul/li[1]/a[contains(text(), 'Arabic')]")).click();
+		
+		
 		
 		//driver.close();
 		//driver.quit();
